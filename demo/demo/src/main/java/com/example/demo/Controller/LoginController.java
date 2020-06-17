7c0bbf0e913b4ab9e74d170dd3fb0bb91a2afcfe
915615783 <915615783@qq.com>
2020-06-17 20:55:35 +0800

    register api done

--- demo/demo/src/main/java/com/example/demo/Controller/LoginController.java
+++ demo/demo/src/main/java/com/example/demo/Controller/LoginController.java
@@ -26,7 +26,7 @@ public class LoginController {
     @CrossOrigin(origins = "*")
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     @ResponseBody
-    public Map login(@RequestParam(value = "username") String username,
+    public Map login(@RequestParam(value = "userid") String userid,
                         @RequestParam(value = "password") String password,
                         @RequestParam(value = "category") String category,
                         HttpSession session) throws Exception {
@@ -34,7 +34,7 @@ public class LoginController {
         List<Seller> sellerList;
         Map<String, String> map = new HashMap<>();
         if(category.equals("customer")){
-            customerList = CM.selectCustomer(username);
+            customerList = CM.selectCustomer(userid);
             if(customerList.size() == 0){
                 map.put("result", "false");
                 return map;
@@ -45,7 +45,7 @@ public class LoginController {
             }
         }
         else if(category.equals("seller")){
-            sellerList = SM.selectSeller(username);
+            sellerList = SM.selectSeller(userid);
             if(sellerList.size() == 0){
                 map.put("result", "false");
                 return map;
@@ -65,56 +65,57 @@ public class LoginController {
 
     }
 
-    @CrossOrigin(origins = "*")
-    @RequestMapping(value = "/login/{username}", method = RequestMethod.GET)
-    @ResponseBody
-    public Map loginGet(@PathVariable String username, HttpSession session) throws Exception {
-        System.out.println(session.getAttribute("isLogIn"));
-        System.out.println(session.getAttribute("isLogIn") == null);
-        session.setAttribute("isLogIn", true);
-        System.out.println(session.getAttribute("isLogIn"));
-        String password = "123";
-        String category = "customer";
-        System.out.println(username);
-        System.out.println(password);
-        System.out.println(category);
-        List<Customer> customerList;
-        List<Seller> sellerList;
-        LoginResponse lr = new LoginResponse();
-        Map<String, String> map = new HashMap();
-        if(category.equals("customer")){
-            customerList = CM.selectCustomer(username);
-            System.out.println(customerList.size());
-            if(customerList.size() == 0){
-                System.out.println(222);
-                lr.setSuccess("false");
-                map.put("success", "false");
-                return map;
-            }
-            if(!customerList.get(0).getPassword().equals(password)){
-                System.out.println(111);
-                System.out.println(customerList.get(0).getPassword());
-                System.out.println(password);
-                System.out.println(password.equals(customerList.get(0).getPassword()));
-                map.put("success", "false");
-                return map;
-            }
-        }
-        else if(category.equals("seller")){
-            sellerList = SM.selectSeller(username);
-            if(sellerList.size() == 0){
-                map.put("success", "false");
-                return map;
-            }
-            if(!sellerList.get(0).getPassword().equals(password)){
-                map.put("success", "false");
-                return map;
-            }
-        }
-        map.put("success", "true");
-        return map;
 
-    }
+//    @CrossOrigin(origins = "*")
+//    @RequestMapping(value = "/login/{username}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public Map loginGet(@PathVariable String username, HttpSession session) throws Exception {
+//        System.out.println(session.getAttribute("isLogIn"));
+//        System.out.println(session.getAttribute("isLogIn") == null);
+//        session.setAttribute("isLogIn", true);
+//        System.out.println(session.getAttribute("isLogIn"));
+//        String password = "123";
+//        String category = "customer";
+//        System.out.println(username);
+//        System.out.println(password);
+//        System.out.println(category);
+//        List<Customer> customerList;
+//        List<Seller> sellerList;
+//        LoginResponse lr = new LoginResponse();
+//        Map<String, String> map = new HashMap();
+//        if(category.equals("customer")){
+//            customerList = CM.selectCustomer(username);
+//            System.out.println(customerList.size());
+//            if(customerList.size() == 0){
+//                System.out.println(222);
+//                lr.setSuccess("false");
+//                map.put("success", "false");
+//                return map;
+//            }
+//            if(!customerList.get(0).getPassword().equals(password)){
+//                System.out.println(111);
+//                System.out.println(customerList.get(0).getPassword());
+//                System.out.println(password);
+//                System.out.println(password.equals(customerList.get(0).getPassword()));
+//                map.put("success", "false");
+//                return map;
+//            }
+//        }
+//        else if(category.equals("seller")){
+//            sellerList = SM.selectSeller(username);
+//            if(sellerList.size() == 0){
+//                map.put("success", "false");
+//                return map;
+//            }
+//            if(!sellerList.get(0).getPassword().equals(password)){
+//                map.put("success", "false");
+//                return map;
+//            }
+//        }
+//        map.put("success", "true");
+//        return map;
+//
+//    }
 
 
 }
